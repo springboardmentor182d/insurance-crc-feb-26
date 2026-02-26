a652fe04c9558748ae548c44ead8bd5db748237b
Suchita-Kumari52 <ksuchita900@gmail.com>
2026-02-26 19:37:52 +0530

    backend work

--- server/src/main.py
+++ server/src/main.py
@@ -12,7 +12,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
