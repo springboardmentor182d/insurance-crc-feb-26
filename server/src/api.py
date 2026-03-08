4e9a9250cd161d9039c8ba8d3d4677aaacc7c3c3
Jaydeep Maharu Mahale <mahalejaydeep95@gmail.com>
2026-03-08 22:53:00 +0530

    Added Policy Catalog API and updated frontend

--- server/src/api.py
+++ server/src/api.py
@@ -2,7 +2,11 @@ from fastapi import APIRouter
 
 from src.auth.controller import router as auth_router
 from src.users.controller import router as users_router
+from src.browse_policies.controller import router as policies_router
+from src.active_policies.controller import router as active_policies_router
 
 api_router = APIRouter()
 api_router.include_router(auth_router, prefix="/auth", tags=["Auth"])
 api_router.include_router(users_router, prefix="/users", tags=["Users"])
+api_router.include_router(policies_router, prefix="/policies", tags=["Policies"])
+api_router.include_router(active_policies_router, prefix="/policies", tags=["Policies"])
