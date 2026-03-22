1a4da759e679c00cfb7f10d105016cc5d67fd70e
meera927472 <meeragbs123@gmail.com>
2026-03-22 12:13:50 +0530

    refactor: restructure frontend and backend into src folders

--- server/src/main.py
+++ server/src/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
-from src.api import router
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.database.core import Base, engine
 from src.users.controller import router as users_router
 from src.auth.controller import router as auth_router
 from src.fraud.controller import router as fraud_router
-from src.fraud import models
+
 app = FastAPI()
 
 # create tables
 Base.metadata.create_all(bind=engine)
 
+# CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -20,11 +20,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Include routers
 app.include_router(users_router)
 app.include_router(auth_router)
 app.include_router(fraud_router)
 
-
 @app.get("/")
 def home():
     return {"message": "FastAPI server running"}
+
+@app.get("/stats")
+def get_stats():
+    return {
+        "approved": 300,
+        "pending": 150,
+        "rejected": 50
+    }
