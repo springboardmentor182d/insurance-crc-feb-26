6b452d4446e8aeb9034a62a70721e8f5a1832de6
Meera927472 <meeragbs123@gmail.com>
2026-03-22 10:59:58 +0530

    Delete server/service.py

--- server/service.py
+++
@@ -1,8 +0,0 @@
-def get_dashboard_data():
-    return {
-        "totalUsers": 15000,
-        "approvedClaims": 2000,
-        "pendingClaims": 400,
-        "underReview": 120,
-        "rejected": 50
-    }
